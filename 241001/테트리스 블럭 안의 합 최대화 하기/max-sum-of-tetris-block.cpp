1b274811b0c32814e214a1389d7b13e5630b6cca
HosungJoo <136783996+HosungJoo@users.noreply.github.com>
2024-10-01 23:50:35 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 21ms, Memory: 0MB, Status: Passed - CodeTree

--- 241001/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.cpp
+++ 241001/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.cpp
@@ -1,147 +1,545 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
 
 using namespace std;
 
 int N, M;
-int map[201][201];
-int ans;
+int map[200][200];
+int ans = 0;
 
-void oneBlock(int x,int y) {
+void line_blk(int x, int y) {
 
-	//오른쪽
-	int Rsum = 0;
-	for (int i = 0; i < 4; ++i) {
-		if (y + 3 >= M) break; //끝이 넘어가면 못함
-		Rsum += map[x][y + i];
-	}
+	int cnt = map[x][y];
+	int nx = x;
+	int ny = y;
 
-	ans = max(ans, Rsum);
-
-	//왼쪽
-	int Lsum = 0;
-	for (int i = 0; i < 4; ++i) {
-		if (y - 3 < 0) break; //끝이 넘어가면 못함
-		Lsum = map[x][y - i];
-	}
-
-	ans = max(ans, Lsum);
-
-	//위
-	int Usum = 0;
-	for (int i = 0; i < 4; ++i) {
-		if (x - 3 < 0) break; //끝이 넘어가면 못함
-		Usum += map[x - i][y];
-	}
+	//오른쪽으로
+	int right = cnt;
+	bool setting = true;
+	int brks = 1;
 	
-	ans = max(ans, Usum);
+	while (1) {
+		ny++;
+		brks++;
 
-	//아래
-	int Dsum = 0;
-	for (int i = 0; i < 4; ++i) {
-		if (x + 3 >= N) break; //끝이 넘어가면 못함
-		Dsum += map[x + i][y];
+		if (ny >= M) {
+			setting = false;
+			break;
+		}
+
+		right += map[nx][ny];
+
+		if (brks == 4) break;
 	}
 
-	ans = max(ans, Dsum);
-	return;
+	if (setting) {
+		ans = max(ans, right);
+	}
+	ny = y;
+
+	//왼쪽으로
+	int left = cnt;
+	setting = true;
+	brks = 1;
+
+	while (1) {
+		ny--;
+		brks++;
+
+		if (ny < 0) {
+			setting = false;
+			break;
+		}
+
+		left += map[nx][ny];
+
+		if (brks == 4) break;
+	}
+
+	if (setting) {
+		ans = max(ans, left);
+	}
+	ny = y;
+
+	//위쪽으로
+	int up = cnt;
+	setting = true;
+	brks = 1;
+
+	while (1) {
+		nx--;
+		brks++;
+
+		if (nx < 0) {
+			setting = false;
+			break;
+		}
+
+		up += map[nx][ny];
+
+		if (brks == 4) break;
+	}
+
+	if (setting) {
+		ans = max(ans, up);
+	}
+	nx = x;
+
+	//아래쪽으로
+	int down = cnt;
+	setting = true;
+	brks = 1;
+
+	while (1) {
+		nx++;
+		brks++;
+
+		if (nx >= N) {
+			setting = false;
+			break;
+		}
+
+		down += map[nx][ny];
+
+		if (brks == 4) break;
+	}
+
+	if (setting) {
+		ans = max(ans, left);
+	}
+	nx = x;
 }
 
-void sQuare(int x, int y) {
-	if (x + 1 >= N || y + 1 >= M) return; //사각형이 넘어감
-	ans = max(ans, map[x][y] + map[x + 1][y] + map[x][y + 1] + map[x + 1][y + 1]);
-	return;
+void cube_blk(int x, int y) {
+	if (x == N - 1 || y == M - 1) return;
+
+	int brk = map[x][y] + map[x + 1][y] + map[x][y + 1] + map[x + 1][y + 1];
+	ans = max(ans, brk);
 }
 
-void ziGzag(int x, int y) {
-	//아래
-	if (x + 2 < N&&y + 1 < M) {
-		ans = max(ans, map[x][y] + map[x + 1][y] + map[x + 1][y + 1] + map[x + 2][y + 1]);
+void L_blk(int x, int y) {
+	int cnt = map[x][y];
+	int nx = x;
+	int ny = y;
+
+	//오른쪽으로
+	int right = cnt;
+	bool setting = true;
+	int brks = 1;
+
+	while (1) {
+		ny++;
+		brks++;
+
+		if (ny >= M) {
+			setting = false;
+			break;
+		}
+
+		right += map[nx][ny];
+
+		if (brks == 3) break;
 	}
 
-	//오른쪽
-	if (x - 1 >= 0 && y + 2 < M) {
-		ans = max(ans, map[x][y] + map[x][y + 1] + map[x - 1][y + 1] + map[x - 1][y + 2]);
+	if (setting) {
+		
+		if (nx-1 >= 0) { //정방향
+			ans = max(ans, right+map[nx-1][ny]);
+		}
+		if (nx + 1 < N) { //대칭
+			ans = max(ans, right + map[nx + 1][ny]);
+		}
+		
 	}
 
-	//위
-	if (x - 2 >= 0 && y - 1 >= 0) {
-		ans = max(ans, map[x][y] + map[x-1][y] + map[x - 1][y - 1] + map[x - 2][y - 1]);
+	nx = x;
+	ny = y;
+
+	//왼쪽으로
+	int left = cnt;
+	setting = true;
+	brks = 1;
+
+	while (1) {
+		ny--;
+		brks++;
+
+		if (ny < 0) {
+			setting = false;
+			break;
+		}
+
+		left += map[nx][ny];
+
+		if (brks == 3) break;
 	}
 
-	//왼쪽
-	if (x + 1 < N&&y - 2 >= 0) {
-		ans = max(ans, map[x][y] + map[x][y-1] + map[x + 1][y - 1] + map[x + 1][y - 2]);
+	if (setting) {
+
+		if (nx - 1 >= 0) { //정방향
+			ans = max(ans, left + map[nx - 1][ny]);
+		}
+		if (nx + 1 < N) { //대칭
+			ans = max(ans, left + map[nx + 1][ny]);
+		}
 	}
-	return;
+
+	nx = x;
+	ny = y;
+
+	//위쪽으로
+	int up = cnt;
+	setting = true;
+	brks = 1;
+
+	while (1) {
+		nx--;
+		brks++;
+
+		if (nx < 0) {
+			setting = false;
+			break;
+		}
+
+		up += map[nx][ny];
+
+		if (brks == 3) break;
+	}
+
+	if (setting) {
+
+		if (ny - 1 >= 0) { //정방향
+			ans = max(ans, up + map[nx][ny-1]);
+		}
+		if (ny + 1 < M) { //대칭
+			ans = max(ans, up + map[nx][ny+1]);
+		}
+	}
+
+	nx = x;
+	ny = y;
+
+	//아래쪽으로
+	int down = cnt;
+	setting = true;
+	brks = 1;
+
+	while (1) {
+		nx++;
+		brks++;
+
+		if (nx >= N) {
+			setting = false;
+			break;
+		}
+
+		down += map[nx][ny];
+
+		if (brks == 3) break;
+	}
+
+	if (setting) {
+
+		if (ny - 1 >= 0) { //정방향
+			ans = max(ans, down + map[nx][ny - 1]);
+		}
+		if (ny + 1 < M) { //대칭
+			ans = max(ans, down + map[nx][ny + 1]);
+		}
+
+	}
 }
 
-void niEun(int x, int y) {
+void N_blk(int x, int y) {
+	int cnt = map[x][y];
+	int nx = x;
+	int ny = y;
 
-	//아래
-	if (x + 2 < N&&y + 1 < M) {
-		ans = max(ans, map[x][y] + map[x + 1][y] + map[x + 2][y] + map[x + 2][y + 1]);
+	//오른쪽으로
+	int right = cnt;
+	bool setting = true;
+	int brks = 1;
+
+	while (1) {
+		ny++;
+		brks++;
+
+		if (ny >= M) {
+			setting = false;
+			break;
+		}
+
+		right += map[nx][ny];
+
+		if (brks == 2) break;
 	}
 
-	//오른쪽
-	if (x - 1 >= 0 && y + 2 < M) {
-		ans = max(ans, map[x][y] + map[x][y+1] + map[x][y+2] + map[x - 1][y + 2]);
+	if (setting) {
+		if (ny + 1 < M) {
+			if (nx - 1 >= 0) { //정방향
+				int r_u = map[nx - 1][ny] + map[nx - 1][ny + 1];
+				ans = max(ans, right + r_u);
+			}
+			if (nx + 1 < N) { //대칭
+				int r_d = map[nx + 1][ny] + map[nx + 1][ny + 1];
+				ans = max(ans, right + r_d);
+			}
+		}
 	}
 
-	//위
-	if (x - 2 >= 0 && y - 1 >= 0) {
-		ans = max(ans, map[x][y] + map[x - 1][y] + map[x - 2][y] + map[x - 2][y - 1]);
+	nx = x;
+	ny = y;
+
+	//왼쪽으로
+	int left = cnt;
+	setting = true;
+	brks = 1;
+
+	while (1) {
+		ny--;
+		brks++;
+
+		if (ny < 0) {
+			setting = false;
+			break;
+		}
+
+		left += map[nx][ny];
+
+		if (brks == 2) break;
 	}
 
-	//왼쪽
-	if (x + 1 < N  && y - 2 >= 0) {
-		ans = max(ans, map[x][y] + map[x][y - 1] + map[x][y - 2] + map[x + 1][y - 2]);
+	if (setting) {
+		if (ny - 1 >= 0) {
+			if (nx - 1 >= 0) { //정방향
+				int l_u = map[nx - 1][ny] + map[nx - 1][ny - 1];
+				ans = max(ans, left + l_u);
+			}
+			if (nx + 1 < N) { //대칭
+				int l_d = map[nx + 1][ny] + map[nx + 1][ny - 1];
+				ans = max(ans, left + l_d);
+			}
+		}
 	}
 
-	return;
+	nx = x;
+	ny = y;
+
+	//위쪽으로
+	int up = cnt;
+	setting = true;
+	brks = 1;
+
+	while (1) {
+		nx--;
+		brks++;
+
+		if (nx < 0) {
+			setting = false;
+			break;
+		}
+
+		up += map[nx][ny];
+
+		if (brks == 2) break;
+	}
+
+	if (setting) {
+		if (nx - 1 >= 0) {
+			if (ny - 1 >= 0) { //정방향
+				ans = max(ans, up + map[nx][ny - 1] + map[nx-1][ny-1]);
+			}
+			if (ny + 1 < M) { //대칭
+				ans = max(ans, up + map[nx][ny + 1] + map[nx-1][ny+1]);
+			}
+		}
+	}
+
+	nx = x;
+	ny = y;
+
+	//아래쪽으로
+	int down = cnt;
+	setting = true;
+	brks = 1;
+
+	while (1) {
+		nx++;
+		brks++;
+
+		if (nx >= N) {
+			setting = false;
+			break;
+		}
+
+		up += map[nx][ny];
+
+		if (brks == 2) break;
+	}
+
+	if (setting) {
+		if (nx + 1 < N) {
+			if (ny - 1 >= 0) { //정방향
+				ans = max(ans, down + map[nx][ny - 1] + map[nx + 1][ny - 1]);
+			}
+			if (ny + 1 < M) { //대칭
+				ans = max(ans, down + map[nx][ny + 1] + map[nx + 1][ny + 1]);
+			}
+		}
+	}
 }
 
-void san(int x, int y) {
-	//아래
-	if (x + 2 < N&&y + 1 < M) {
-		ans = max(ans, map[x][y] + map[x + 1][y] + map[x + 1][y + 1] + map[x + 2][y]);
+void Y_blk(int x, int y) {
+	int cnt = map[x][y];
+	int nx = x;
+	int ny = y;
+
+	//오른쪽으로
+	int right = cnt;
+	bool setting = true;
+	int brks = 1;
+
+	while (1) {
+		ny++;
+		brks++;
+
+		if (ny >= M) {
+			setting = false;
+			break;
+		}
+
+		right += map[nx][ny];
+
+		if (brks == 3) break;
 	}
 
-	//오른쪽
-	if (x - 1 >= 0 && y + 2 < M) {
-		ans = max(ans, map[x][y] + map[x][y + 1] + map[x-1][y + 1] + map[x][y + 2]);
+	if (setting) {
+
+		if (nx - 1 >= 0) { //정방향
+			ans = max(ans, right + map[nx - 1][ny-1]);
+		}
+		if (nx + 1 < N) { //대칭
+			ans = max(ans, right + map[nx + 1][ny-1]);
+		}
+
 	}
 
-	//위
-	if (x - 2 >= 0&&y - 1 >= 0) {
-		ans = max(ans, map[x][y] + map[x - 1][y] + map[x - 1][y - 1] + map[x - 2][y]);
+	nx = x;
+	ny = y;
+
+	//왼쪽으로
+	int left = cnt;
+	setting = true;
+	brks = 1;
+
+	while (1) {
+		ny--;
+		brks++;
+
+		if (ny < 0) {
+			setting = false;
+			break;
+		}
+
+		left += map[nx][ny];
+
+		if (brks == 3) break;
 	}
 
-	//왼쪽
-	if (x + 1 < N&&y - 2 >= 0) {
-		ans = max(ans, map[x][y] + map[x][y - 1] + map[x + 1][y - 1] + map[x][y - 2]);
+	if (setting) {
+
+		if (nx - 1 >= 0) { //정방향
+			ans = max(ans, left + map[nx - 1][ny+1]);
+		}
+		if (nx + 1 < N) { //대칭
+			ans = max(ans, left + map[nx + 1][ny+1]);
+		}
 	}
-	return;
+
+	nx = x;
+	ny = y;
+
+	//위쪽으로
+	int up = cnt;
+	setting = true;
+	brks = 1;
+
+	while (1) {
+		nx--;
+		brks++;
+
+		if (nx < 0) {
+			setting = false;
+			break;
+		}
+
+		up += map[nx][ny];
+
+		if (brks == 3) break;
+	}
+
+	if (setting) {
+
+		if (ny - 1 >= 0) { //정방향
+			ans = max(ans, up + map[nx+1][ny - 1]);
+		}
+		if (ny + 1 < M) { //대칭
+			ans = max(ans, up + map[nx+1][ny + 1]);
+		}
+	}
+
+	nx = x;
+	ny = y;
+
+	//아래쪽으로
+	int down = cnt;
+	setting = true;
+	brks = 1;
+
+	while (1) {
+		nx++;
+		brks++;
+
+		if (nx >= N) {
+			setting = false;
+			break;
+		}
+
+		down += map[nx][ny];
+
+		if (brks == 3) break;
+	}
+
+	if (setting) {
+
+		if (ny - 1 >= 0) { //정방향
+			ans = max(ans, down + map[nx-1][ny - 1]);
+		}
+		if (ny + 1 < M) { //대칭
+			ans = max(ans, down + map[nx-1][ny + 1]);
+		}
+
+	}
 }
 
-int main()
-{
+int main() {
+
 	cin >> N >> M;
 
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < M; ++j) {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
 			cin >> map[i][j];
 		}
 	}
 
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < M; ++j) {
-			
-			oneBlock(i, j); //일자 블럭
-			sQuare(i, j); //사각 블럭
-			ziGzag(i, j); //지그재그 블럭
-			niEun(i, j); //니은 블럭
-			san(i, j); //산 블럭
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+
+			line_blk(i, j);
+			cube_blk(i, j);
+			L_blk(i, j);
+			N_blk(i, j);
+			Y_blk(i, j);
+
 		}
 	}
 
